f12de775a208bcb1f1c7a7864f470506e1938dd7
Tagir Almasov <tccup1@gmail.com>
2021-07-04 15:05:44 +0000

    add headless ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
 import Head from "next/head"
+import { useState } from "react"
+
+import { Popup, State as PopupState, Msg as PopupMsg } from "@app/components/Popup"
+import { nr } from "@app/utils"
 
 export default function Home() {
+  const [popupState, setPopupState] = useState<PopupState>({ type: "closed" })
+
+  const popupHandler = (msg: PopupMsg) => {
+    switch (msg.type) {
+      case "close_clicked":
+        setPopupState({ type: "closed" })
+        break
+      case "ok_clicked":
+        setPopupState({ type: "closed" })
+        break
+      default:
+        nr(msg)
+    }
+  }
   return (
     <div>
       <Head>
@@ -9,6 +27,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+      <Popup state={popupState} onMsg={popupHandler} />
     </div>
   )
 }
