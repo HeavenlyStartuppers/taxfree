ab8952458b9ff38333015d5ad2d509058b3c9680
Tagir Almasov <tccup1@gmail.com>
2021-11-04 10:12:16 +0000

    chore: deploy trigger

--- domains/FAQ/data/faqList.ts
+++ domains/FAQ/data/faqList.ts
@@ -3,7 +3,7 @@ import { FAQ } from "../FAQ"
 export const faqList: FAQ[] = [
   {
     question: "За какие товары можно вернуть налог?",
-    answer: "Зависит от страны",
+    answer: "Зависит от страны, лол",
   },
   {
     question: "Кто может вернуть налог?",
